033b8f563076f5843d64aaccac2f7f805c5cff93
Quentin Vekeman-Marcelis <quentin.vekeman-marcelis@student.ehb.be>
2019-02-28 00:15:50 +0100

    Finished prototype

---
+++ Consumer/Program.cs
@@ -0,0 +1,47 @@
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "exchange_kassa", type: "topic");
+
+                channel.QueueDeclare(queue: "queue_planning",
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+                channel.QueueBind(queue: "queue_planning",
+                                  exchange: "exchange_kassa",
+                                  routingKey: "guest.checkIn");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+                channel.BasicConsume(queue: "queue_planning",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
--- Producer/Producer.cs
+++ Producer/Producer.cs
@@ -1,3 +1,4 @@
+using RabbitMQ.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,47 @@ namespace Producer
 {
     class Producer
     {
+        //TODO: Config file voor hardcoded namen
         static void Main(string[] args)
         {
+
+            //Per exchange moet een nieuwe connection aangemaakt worden
+            //TODO: methode og apparte klasse voor schrijven (indien mogelijk)
+            var factory = new ConnectionFactory();
+
+            //Vervang localhost door het address waar de RabbitMQ server op draait
+            factory.HostName = "localhost";
+
+            //Vervang exchange door de vooraf afgesproken naam van eigen exchange(bvb exchange_kassa)
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "exchange_kassa",
+                                        type: "topic");
+
+                SendMessage("Hello world", "guest.checkIn", channel);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+
             
+        }
+
+        //Vervang string door byte[] voor xml en verwijder de conversie
+        static void SendMessage(String message, String key, IModel channel)
+        {
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
+
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "exchange_kassa",
+                                 routingKey: key,
+                                 basicProperties: properties,
+                                 body: body);
+
+            Console.WriteLine(" [x] Sent {0}", message);
         }
     }
 }
